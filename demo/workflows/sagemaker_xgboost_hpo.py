7201276e013d65bc1991ea3e878d249b406d5d71
Chang-Hong Hsu <changhonghsu@lyft.com>
2020-04-13 16:12:25 -0700

    correct memory limit setting from 200mi to 500mi

--- demo/workflows/sagemaker_xgboost_hpo.py
+++ demo/workflows/sagemaker_xgboost_hpo.py
@@ -70,7 +70,7 @@ def read_and_merge(first, second):
 
 @inputs(x_train=Types.Schema(), x_test=Types.Schema(), y_train=Types.Schema(), y_test=Types.Schema())
 @outputs(train=Types.MultiPartCSV, validation=Types.MultiPartCSV)
-@python_task(cache_version='3.0', cache=True, memory_limit="200Mi")
+@python_task(cache_version='3.0', cache=True, memory_limit="500Mi")
 def convert_to_sagemaker_csv(ctx, x_train, y_train, x_test, y_test, train, validation):
     _train = read_and_merge(y_train, x_train)
     _validate = read_and_merge(y_test, x_test)
@@ -88,7 +88,7 @@ def convert_to_sagemaker_csv(ctx, x_train, y_train, x_test, y_test, train, valid
 
 @inputs(model_tar=Types.Blob)
 @outputs(model=Types.Blob)
-@python_task(cache_version="3.0", cache=True, memory_limit="200Mi")
+@python_task(cache_version="3.0", cache=True, memory_limit="500Mi")
 def untar_xgboost(ctx, model_tar, model):
     model_tar.download()
     fname = "xgboost-model"
